fe1ba4c555278b91891b1da2be8d19d78f049bb2
Adalbert Barta <adalbertbarta@gmail.com>
2023-06-20 15:15:39 +0200

    kubeconfig google

--- outputs.tf
+++ outputs.tf
@@ -1,18 +1,23 @@
 # ${path.module} is an interpolated string in Terraform, which references the path to the current
 # module. In this case, it returns the path to the directory containing the current module.
+resource "local_file" "kubeconfig" {
+  content  = module.gke_auth.kubeconfig_raw
+  filename = "${path.module}/kubeconfig"
+  file_permission = "0400"
+}
 
-data "local_file" "kubeconfig" {
-  depends_on = [module.gke_auth]
+data "local_file" "kubeconfig_data" {
+  depends_on = [local_file.kubeconfig]
   filename = "${path.module}/kubeconfig"
 }
 
-output "kubeconfig" {
-  value       = "${path.module}/kubeconfig"
+output "kubeconfig_path" {
+  value       = local_file.kubeconfig.filename
   description = "The path to the kubeconfig file"
 }
 
 output "kubeconfig_content" {
   description = "The content of the kubeconfig file for the created cluster."
-  value       = data.local_file.kubeconfig.content
+  value       = data.local_file.kubeconfig_data.content
   sensitive   = true
 }
